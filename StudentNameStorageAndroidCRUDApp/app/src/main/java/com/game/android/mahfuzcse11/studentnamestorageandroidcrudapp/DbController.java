d237fa873e833619bb8176e40941be20a0025186
6895mahfuz@gmail.com <6895mahfuz@gmail.com>
2017-06-19 21:28:37 +0600

    Added DbController and CRUD operation methods

---
+++ StudentNameStorageAndroidCRUDApp/app/src/main/java/com/game/android/mahfuzcse11/studentnamestorageandroidcrudapp/DbController.java
@@ -0,0 +1,71 @@
+package com.game.android.mahfuzcse11.studentnamestorageandroidcrudapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.TextView;
+
+/**
+ * Created by MahfuzCSE'11 on 19-Jun-17.
+ */
+
+public class DbController extends SQLiteOpenHelper {
+
+
+    public DbController(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, "Test.db", factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("CREATE TABLE STUDENTS(ID INTEGER PRIMARY KEY AUTOINCREMENT,FIRSTNAME TEXT UNIQUE,LASTNAME TEXT UNIQUE);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE  IF EXISTS  STUDENTS ");
+        onCreate(db);
+
+    }
+
+
+    public void insertStudent(String firstName, String lastName) {
+
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("FIRSTNAME", firstName);
+        contentValues.put("LASTNAME", lastName);
+        this.getWritableDatabase().insertOrThrow("STUDENTS", "", contentValues);
+
+    }
+
+
+    public void deleteStudent(String firstName) {
+        this.getWritableDatabase().delete("STUDENTS", "FIRSTNAME= '" + firstName + "' ", null);
+    }
+
+    public void updateStudent(String oldFirstName, String newFirstName) {
+        this.getWritableDatabase().execSQL("UPDATE  STUDENTS SET FIRSTNAME='" + newFirstName + "' WHERE FIRSTNAME='" + oldFirstName + "'");
+    }
+
+    public void listAllStudent(TextView textView) {
+
+        Cursor cursor = this.getReadableDatabase().rawQuery("SELECT * FROM  STUDENTS", null);
+
+
+        textView.setText("");
+        while (cursor.moveToNext()) {
+
+
+            textView.append(cursor.getString(1) + "   " + cursor.getString(2));
+        }
+
+
+    }
+
+
+}
